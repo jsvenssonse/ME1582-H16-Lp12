1abf5eea64740bf812e325579f2a18286b5e3c14
kevintatou <kevintatou94@gmail.com>
2016-09-14 08:52:13 +0100

    added images and itemsarray

--- entangled/js/inventory.js
+++ entangled/js/inventory.js
@@ -6,14 +6,33 @@ function inventory(item) {
        "name" : item
     }]; 
 
-var setCookie = Cookies.set("name", saveInventory, {expires : 0.00001});
+var setCookie = Cookies.set("name", saveInventory);
 
 
     return Cookies.get("name");
 }
 
+//Static items for the game
+function itemArray() {
+    var itemArray;
+    itemArray = [
+    {
+       "name" : "keyOne",
+       "img" : "../graphics/nyckel1.png"
+    },
+    {
+       "name" : "keyTwo",
+       "img" : "../graphics/nyckel2.png"
+    }
+    ]; 
 
-console.log(inventory("item"));
+
+    return itemArray;
+}
+for (i in itemArray()) {
+    $('#items').append('<img src="'+ itemArray()[i].img  +'" />');
+}
+
 });
 
 
